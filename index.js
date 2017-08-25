b54f23f6affe6c834ad11116d6eb5c30d6b4dc8e
ethanryan <ethanryan@gmail.com>
2017-08-25 19:23:53 -0400

    fixed navbar mobile collapse using jquery instead of other way, which didn't work

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//collapse navbar onclick on mobile
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+
 // Define initial variables.
 var words = [
 "capable",
